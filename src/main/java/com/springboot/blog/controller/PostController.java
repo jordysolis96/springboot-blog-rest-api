f24d8dea198ea46527ace9f22c4c4ca57641d652
jordy muniz <jordy.muniz96@gmail.com>
2023-03-02 09:52:56 -0600

    semi functioning version

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -7,6 +7,7 @@ import com.springboot.blog.utils.AppConstants;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +24,8 @@ public class PostController {
 
     // create blog post rest api
     // @Valid annotation will ensure validation for RequestBody
+    // only Admin can access this api endpoint
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.creatPost(postDto), HttpStatus.CREATED);
@@ -51,6 +54,8 @@ public class PostController {
 
     // update post by id rest api
     // @Valid annotation will ensure validation for RequestBody
+    // only Admin can access this api endpoint
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id")long id){
         PostDto postResponse = postService.updatePost(postDto, id);
@@ -58,6 +63,8 @@ public class PostController {
     }
 
     // delete post by id rest api
+    // only Admin can access this api endpoint
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@PathVariable(name = "id")long id){
         postService.deletePostById(id);
